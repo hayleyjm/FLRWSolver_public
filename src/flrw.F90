32bbb6b4a987b8ef6a98eac18f3ae574888e968a
Hayley Macpherson <hayley.macpherson@monash.edu>
2015-08-26 18:22:46 +1000

    un-did all hacks, added new parameters for different types of perturbation to make life alot easier

--- src/flrw.F90
+++ src/flrw.F90
@@ -15,9 +15,9 @@ subroutine FLRW_InitialData (CCTK_ARGUMENTS)
   DECLARE_CCTK_FUNCTIONS
   DECLARE_CCTK_PARAMETERS
   integer   :: i, j, k
-  real :: a0, kvalue, asq, adot, rho0, r_gauss, r0, perturb_rho0, box_length, phi, tophat_rad, rad, kx
+  real :: a0, kvalue, asq, adot, rho0, r_gauss, r0, perturb_rho0, box_length, phi, rad, kx
   real, parameter :: pi = 3.14159265358979323846264338327
-  real :: P, Q, W, B
+  real :: P, Q, W, offset_x, offset_y, offset_z
   logical   :: lapse, dtlapse, shift, data, hydro
   lapse = CCTK_EQUALS (initial_lapse, "flrw")
   dtlapse = CCTK_EQUALS (initial_dtlapse, "flrw")
@@ -27,56 +27,86 @@ subroutine FLRW_InitialData (CCTK_ARGUMENTS)
 
   rho0 = FLRW_init_rho
   perturb_rho0 = FLRW_pert_amplitude*rho0
-  r0 = Gaussian_r0
+  r0 = FLRW_radius
   a0 = 1.0
   asq = a0*a0
   adot = sqrt((8.0 * pi)*rho0*asq/3.0)
   kvalue = -adot * a0
-  box_length = 480.
-  tophat_rad = 150.
+  box_length = 2.0 * FLRW_xmax
+  offset_x = FLRW_offset_x
+  offset_y = FLRW_offset_y
+  offset_z = FLRW_offset_z
   kx = 2.0*pi/box_length
-  B = 1.e-8	!!phi amplitude
+
 
   do k = 1, cctk_lsh(3)
     do j = 1, cctk_lsh(2)
       do i = 1, cctk_lsh(1)
 
-      	 !! offset radius for tophat perturbation (boundary test)
-         rad = sqrt((x(i,j,k) - 50.)**2 + y(i,j,k)**2 + z(i,j,k)**2)
-
 	 !! set phi depending on perturbation
+
 	 if (CCTK_EQUALS (FLRW_perturb_type, "Sine")) then
-!!	    phi = B*sin(kx*x(i,j,k))
-	    phi = -4.*pi*perturb_rho0*sin(kx*x(i,j,k))/kx**2
+
+	    	if (CCTK_EQUALS (FLRW_phi_type, "General")) then
+	       	   phi = perturb_rho0*sin(kx*x(i,j,k))
+	    	elseif (CCTK_EQUALS (FLRW_phi_type, "Poisson")) then	    
+	       	   phi = -4.*pi*perturb_rho0*sin(kx*x(i,j,k))/kx**2
+	    	endif
+
 	 elseif (CCTK_EQUALS (FLRW_perturb_type, "Tophat")) then
-	 	if (rad <= tophat_rad) then
-         	   phi = 2.*pi* (rho0 + perturb_rho0)*(rad**2 - 3.*tophat_rad**2)/3.
+
+	 	rad = sqrt((x(i,j,k) - offset_x)**2 + (y(i,j,k) - offset_y)**2 + (z(i,j,k) - offset_z)**2)
+	 	
+		if (rad <= r0) then
+         	   phi = 2.*pi* (rho0 + perturb_rho0)*(rad**2 - 3.*r0**2)/3.
 		else
-		   phi = -4.*pi*tophat_rad**2*rho0/3.
+		   phi = -4.*pi*r0**2*rho0/3.
 		endif
 	endif
         
 	 !! set up metric, extrinsic curvature, lapse and shift
 
 	 if (data) then
-            gxx(i,j,k) = asq*(1.0 - 2.0*phi)
-            gxy(i,j,k) = 0.0
-            gxz(i,j,k) = 0.0
-            gyy(i,j,k) = asq*(1.0 - 2.0*phi)
-            gyz(i,j,k) = 0.0
-            gzz(i,j,k) = asq*(1.0 - 2.0*phi)
+	    
+	    if (FLRW_perturb_metric) then
+              gxx(i,j,k) = asq*(1.0 - 2.0*phi)
+              gxy(i,j,k) = 0.0
+              gxz(i,j,k) = 0.0
+              gyy(i,j,k) = asq*(1.0 - 2.0*phi)
+              gyz(i,j,k) = 0.0
+              gzz(i,j,k) = asq*(1.0 - 2.0*phi)
             
-            kxx(i,j,k) = kvalue*(1.0 - 2.0*phi)/sqrt(1.0 + 2.0*phi)
-            kxy(i,j,k) = 0.0
-            kxz(i,j,k) = 0.0
-            kyy(i,j,k) = kvalue*(1.0 - 2.0*phi)/sqrt(1.0 + 2.0*phi)
-            kyz(i,j,k) = 0.0
-            kzz(i,j,k) = kvalue*(1.0 - 2.0*phi)/sqrt(1.0 + 2.0*phi)
-         end if
+              kxx(i,j,k) = kvalue*(1.0 - 2.0*phi)/sqrt(1.0 + 2.0*phi)
+              kxy(i,j,k) = 0.0
+              kxz(i,j,k) = 0.0
+              kyy(i,j,k) = kvalue*(1.0 - 2.0*phi)/sqrt(1.0 + 2.0*phi)
+              kyz(i,j,k) = 0.0
+              kzz(i,j,k) = kvalue*(1.0 - 2.0*phi)/sqrt(1.0 + 2.0*phi)
+            
+	    else
+	      gxx(i,j,k) = asq
+              gxy(i,j,k) = 0.0
+              gxz(i,j,k) = 0.0
+              gyy(i,j,k) = asq
+              gyz(i,j,k) = 0.0
+              gzz(i,j,k) = asq
+
+              kxx(i,j,k) = kvalue
+              kxy(i,j,k) = 0.0
+              kxz(i,j,k) = 0.0
+              kyy(i,j,k) = kvalue
+              kyz(i,j,k) = 0.0
+              kzz(i,j,k) = kvalue
+	    endif
+	 end if
 
          if (lapse) then
-            alp(i,j,k) = sqrt(1.0 + 2.0*phi)
-         end if
+	    if (FLRW_perturb_metric) then
+               alp(i,j,k) = sqrt(1.0 + 2.0*phi)
+   	    else
+	       alp(i,j,k) = 1.0
+            endif
+	 end if
 
          ! May also need the derivative of the lapse -- this is specified in ADMBase (somehow).
          if (dtlapse) then
@@ -100,29 +130,53 @@ subroutine FLRW_InitialData (CCTK_ARGUMENTS)
             vel(i,j,k,3) = 0.0
          end if
          
-	 P = B**2*kx**2
-	 Q = B*kx**2
-	 W = sqrt(2.*pi*rho0/3.)
+	 !! set density depending on perturbation
 
-	if (FLRW_perturb) then
+	if (FLRW_perturb_density) then
+
 	    if (CCTK_EQUALS (FLRW_perturb_type, "Gaussian")) then
-		r_gauss = sqrt(x(i,j,k)**2 + y(i,j,k)**2 + z(i,j,k)**2)
-		rho(i,j,k) = rho(i,j,k) + perturb_rho0*exp(-r_gauss**2/r0**2)
+		
+		rad = sqrt((x(i,j,k) - offset_x)**2 + (y(i,j,k) - offset_y)**2 + (z(i,j,k) - offset_z)**2)
+
+		rho(i,j,k) = rho(i,j,k) + perturb_rho0*exp(-rad**2/r0**2)
+
 	    elseif (CCTK_EQUALS (FLRW_perturb_type, "Sine")) then
+	    	
+		if (CCTK_EQUALS (FLRW_phi_type, "General")) then
+	      	   
+			P = perturb_rho0**2*kx**2
+	    	   	Q = perturb_rho0*kx**2
+            	   	W = sqrt(2.*pi*rho0/3.)
 
-!! 	    below is density satisfying hamiltonian constraint
-!!	    	rho(i,j,k) = (6.*(-2.*W + 4.*B*W*sin(kx*x(i,j,k)))**2) / ((-1. + 2.*B*sin(kx*x(i,j,k)))**2 * (1. + 2.*B*sin(kx*x(i,j,k)))) + (2.*(-3.*P*cos(kx*x(i,j,k))**2 + 2.*Q*sin(kx*x(i,j,k)) - 4.*P*sin(kx*x(i,j,k))**2)) / ((-1 + 2.*B*sin(kx*x(i,j,k)))**3))/(16.pi)
+	    	   	rho(i,j,k) = (6.*(-2.*W + 4.*perturb_rho0*W*sin(kx*x(i,j,k)))**2) / ((-1. + 2.*perturb_rho0*sin(kx*x(i,j,k)))**2 * (1. + 2.*perturb_rho0*sin(kx*x(i,j,k)))) + (2.*(-3.*P*cos(kx*x(i,j,k))**2 + 2.*Q*sin(kx*x(i,j,k)) - 4.*P*sin(kx*x(i,j,k))**2)) / ((-1 + 2.*perturb_rho0*sin(kx*x(i,j,k)))**3)/(16*pi)
 
-!! 		below is sine wave density perturbation
-	        rho(i,j,k) = rho(i,j,k) + perturb_rho0*sin(kx*x(i,j,k))
+		elseif (CCTK_EQUALS (FLRW_phi_type, "Poisson")) then
+
+	            rho(i,j,k) = rho(i,j,k) + perturb_rho0*sin(kx*x(i,j,k))
+
+		endif
 
 	    elseif (CCTK_EQUALS (FLRW_perturb_type, "Tophat")) then
-	    	if (rad < tophat_rad) then
+
+	    	if (rad < r0) then
 		   rho(i,j,k) = rho(i,j,k) + perturb_rho0
 		else
 		   rho(i,j,k) = rho(i,j,k)
 		endif   
+
 	    endif
+
+	    ! COPY TEST DENSITY HERE - SO AS TO NOT DISTURB OTHER DENSITIES
+	    !! current test: density/4pi
+
+	    if (FLRW_test) then
+
+	       rho(i,j,k) = ((6.*(-2.*W + 4.*perturb_rho0*W*sin(kx*x(i,j,k)))**2) / ((-1. + 2.*perturb_rho0*sin(kx*x(i,j,k)))*\
+*2 * (1. + 2.*perturb_rho0*sin(kx*x(i,j,k)))) + (2.*(-3.*P*cos(kx*x(i,j,k))**2 + 2.*Q*sin(kx*x(i,j,k)) - 4.*P*sin(kx*x(i,j,k))**2)) / \
+((-1 + 2.*perturb_rho0*sin(kx*x(i,j,k)))**3)/(16*pi) - rho0)/(4.0*pi) + rho0
+
+	    endif
+
 	endif
 	
 	
