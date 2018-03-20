99da79b46a5eb021f17dff5eecad5813f64c1167
Hayley Macpherson <hayley.macpherson@monash.edu>
2018-03-20 09:35:20 +0800

    cleaned up flrw.F90, param.ccl. added modules required for a build on Magnus

--- src/flrw.F90
+++ src/flrw.F90
@@ -20,7 +20,7 @@ subroutine FLRW_InitialData (CCTK_ARGUMENTS)
   real(dp) :: rho0, rhostar, perturb_rho0, perturb_v0
   real(dp) :: box_length_x, box_length_y, box_length_z, box_length, wavelength
   real(dp) :: perturb_phi, phidot, phi_offset, amp
-  real(dp), parameter :: pi = 3.14159265358979323846264338327
+  real(dp), parameter :: pi = 4.*atan(1.)
   real(dp) :: f, df1, df2, df3
   real(dp) :: kx, ky, kz, modk, kval
   real(dp) :: xmin, xmax
@@ -30,13 +30,13 @@ subroutine FLRW_InitialData (CCTK_ARGUMENTS)
   real(dp), dimension(cctk_lsh(1),cctk_lsh(2),cctk_lsh(3)) :: phi, delta
   real(dp), dimension(cctk_lsh(1),cctk_lsh(2),cctk_lsh(3),3) :: delta_vel
 
-  logical   :: lapse, dtlapse, shift, data, hydro, perturb_x, perturb_y, perturb_z, perturb_all, cmb_like, single_mode
+  logical   :: lapse, dtlapse, shift, data, hydro, perturb_x, perturb_y, perturb_z, perturb_all,&
+       cmb_like, single_mode, perturb
   integer :: dr_unit, dv_unit1, dv_unit2, dv_unit3, p_unit
-  integer :: res, il,jl,kl, iu,ju,ku  ! resolution, indices of upper/lower bounds of local grid within global grid
-  character(len=40) :: deltafile, vel1file, vel2file, vel3file, phifile
-  
-  !! test print CoordBase variables - didnt work. put inherits:: Coordbase into interface.ccl, didnt like that...
-!  print*,'your xmax is',xmax,'your xmin is',xmin
+  integer :: res,il,jl,kl,iu,ju,ku
+  character(len=100) :: deltafile, vel1file, vel2file, vel3file, phifile
+  character(len=100) :: dir, ics_dir, ics_type
+  integer :: ics_dir_len, ics_type_len
 
   !
   ! set logicals
@@ -52,12 +52,11 @@ subroutine FLRW_InitialData (CCTK_ARGUMENTS)
   perturb_all = CCTK_EQUALS (FLRW_perturb_direction, "all")
   single_mode = CCTK_EQUALS (FLRW_perturb_type, "single_mode")
   cmb_like = CCTK_EQUALS (FLRW_perturb_type, "CMB_like")
+  perturb = CCTK_EQUALS (FLRW_perturb, "yes")
 
   !
   ! set parameters
   !
-  !z0 = FLRW_initial_redshift
-  !a0 =1._dp / (1._dp + z0)     !! want to include redshift in future. for now cannot simulate other then a0=1
   a0 = 1._dp
   rho0 = FLRW_init_rho
   asq = a0*a0
@@ -66,8 +65,10 @@ subroutine FLRW_InitialData (CCTK_ARGUMENTS)
   kvalue = -adot * a0
   res = int(FLRW_resolution)
 
-  if (single_mode .and. FLRW_perturb) then
+  if (perturb .and. single_mode) then
+     !
      ! set parameters only required for single mode
+     !
      amp = phi_perturb_amplitude
      perturb_phi = amp * rho0
      phi_offset = phi_phase_offset
@@ -78,18 +79,10 @@ subroutine FLRW_InitialData (CCTK_ARGUMENTS)
      kz = 2.0_dp*pi/wavelength
      modk = sqrt(kx**2 + ky**2 + kz**2)
 
+     !
      ! set density, velocity amplitudes
-     if (perturb_x) then
-        kval = kx
-     elseif (perturb_y) then
-        kval = ky
-     elseif (perturb_z) then
-        kval = kz
-     elseif (perturb_all) then
-        kval = modk
-     endif
-!     perturb_rho0 = kval**2 / (4._dp * pi * rho0 * asq) - 2._dp
-     perturb_rho0 = - kx**2 / (4._dp * pi * rho0 * asq) - 2._dp  !! testing - this works. use kx rather than modk!
+     !
+     perturb_rho0 = - kx**2 / (4._dp * pi * rho0 * asq) - 2._dp
      perturb_v0 = - sqrt(a0 / ( 6._dp * pi * rhostar ))        
 
      delta_vel = 0._dp
@@ -97,67 +90,86 @@ subroutine FLRW_InitialData (CCTK_ARGUMENTS)
      phi = 0._dp   
   endif
 
-  if (FLRW_perturb .and. cmb_like) then
-        print*,'cmb_like and FLRW_perturb set to yes.'
+  if (perturb .and. cmb_like) then
+     !
+     ! convert CCTK_STRING "describe_ics" + "FLRW_ICs_dir" to Fortran string
+     !
+     call CCTK_FortranString(ics_type_len,describe_ics,ics_type)
+     call CCTK_FortranString(ics_dir_len,FLRW_ICs_dir,ics_dir)
+     write(dir,'(a,i3.3,a)')trim(ics_dir),res,trim(ics_type)
+     !print*,trim(dir)
+ 
+     !
+     ! filenames to read ICs from, based on FLRW_ICs_dir + describe_ics parameters + res
+     !
+     write(deltafile,'(a,a)')trim(dir),'/delta.dat'
+     write(vel1file,'(a,a)')trim(dir),'/vel1.dat'
+     write(vel2file,'(a,a)')trim(dir),'/vel2.dat'
+     write(vel3file,'(a,a)')trim(dir),'/vel3.dat'
+     write(phifile,'(a,a)')trim(dir),'/phi.dat'
 
-        ! define filenames to read from, based on resolution
-        ! !! assumes uniform grid !!
-        write(deltafile,'(a,i3.3,a)')'ics_files/ics_files',res,'/delta.dat'
-        write(vel1file,'(a,i3.3,a)')'ics_files/ics_files',res,'/vel1.dat'
-        write(vel2file,'(a,i3.3,a)')'ics_files/ics_files',res,'/vel2.dat'
-        write(vel3file,'(a,i3.3,a)')'ics_files/ics_files',res,'/vel3.dat'
-        write(phifile,'(a,i3.3,a)')'ics_files/ics_files',res,'/phi.dat'
+     !
+     ! open files to read phi, delta, vel perturbs from files
+     !
+     open(newunit=dr_unit,file=deltafile,status='old')  ! delta rho file
+     open(newunit=dv_unit1,file=vel1file,status='old')  ! delta vel file [1]
+     open(newunit=dv_unit2,file=vel2file,status='old')  ! delta vel file [2]
+     open(newunit=dv_unit3,file=vel3file,status='old')  ! delta vel file [3] 
+     open(newunit=p_unit,file=phifile,status='old')     ! phi file
+     print*,'opened IC files'
 
-        ! open files to read phi, delta, vel perturbs from files
-        open(newunit=dr_unit,file=deltafile,status='old')  ! delta rho file
-        open(newunit=dv_unit1,file=vel1file,status='old')  ! delta vel file [1]
-        open(newunit=dv_unit2,file=vel2file,status='old')  ! delta vel file [2]
-        open(newunit=dv_unit3,file=vel3file,status='old')  ! delta vel file [3] 
-        open(newunit=p_unit,file=phifile,status='old')     ! phi file
+     do k = 1, cctk_gsh(3)
+        do j = 1, cctk_gsh(2) 
+           ! loop over ROW no. (i is COLUMN no.) (i,j,k) --> (column, row, z)
 
-        print*,'opened IC files'
-
-        do k = 1, cctk_gsh(3)
-           do j = 1, cctk_gsh(2) ! loop over ROW no. (i is COLUMN no.) (i,j,k) --> (column, row, z)
-              ! read perturbations from CMB-like generated data files to GLOBAL sized arrays
-              read(dr_unit,*) delta_gs(:,j,k)
-              read(dv_unit1,*) delta_vel_gs(:,j,k,1)
-              read(dv_unit2,*) delta_vel_gs(:,j,k,2)
-              read(dv_unit3,*) delta_vel_gs(:,j,k,3)
-              read(p_unit,*), phi_gs(:,j,k)
-           enddo
+           !
+           ! read perturbations from CMB-like ICs files to global sized (gs) arrays
+           !
+           read(dr_unit,*) delta_gs(:,j,k)
+           read(dv_unit1,*) delta_vel_gs(:,j,k,1)
+           read(dv_unit2,*) delta_vel_gs(:,j,k,2)
+           read(dv_unit3,*) delta_vel_gs(:,j,k,3)
+           read(p_unit,*) phi_gs(:,j,k)
         enddo
+     enddo
+     
+     !
+     ! indices for lower bound of local (processor) grid within global grid
+     !
+     il = cctk_lbnd(1) + 1
+     jl = cctk_lbnd(2) + 1  ! indices output from cctk_lbnd start at 0, need to +1
+     kl = cctk_lbnd(3) + 1
+     !
+     ! indices for upper bound of local grid within global grid
+     !
+     iu = cctk_ubnd(1) + 1
+     ju = cctk_ubnd(2) + 1
+     ku = cctk_ubnd(3) + 1
+     !
+     ! extract local part of global grid 
+     !
+     delta = delta_gs(il:iu, jl:ju, kl:ku)
+     delta_vel = delta_vel_gs(il:iu, jl:ju, kl:ku, :)
+     phi = phi_gs(il:iu, jl:ju, kl:ku)
        
-        ! indices for lower bound of local grid within global grid
-        il = cctk_lbnd(1) + 1
-        jl = cctk_lbnd(2) + 1  ! these indices start at 0
-        kl = cctk_lbnd(3) + 1
-        ! indices for upper bound of local grid within global grid
-        iu = cctk_ubnd(1) + 1
-        ju = cctk_ubnd(2) + 1
-        ku = cctk_ubnd(3) + 1
-
-        ! extract local part of global grid 
-        delta = delta_gs(il:iu, jl:ju, kl:ku)
-        delta_vel = delta_vel_gs(il:iu, jl:ju, kl:ku, :)
-        phi = phi_gs(il:iu, jl:ju, kl:ku)
-       
-     endif
-
+  endif
 
   do k = 1, cctk_lsh(3)
      do j = 1, cctk_lsh(2)
         do i = 1, cctk_lsh(1)
 
-           if (single_mode .and. FLRW_perturb) then
-              ! set single mode perturbation in phi and d(phi)/dx^i -- used for delta and delta_vel
+           if (perturb .and. single_mode) then
+              !
+              ! set single mode perturbation in phi and d(phi)/dx^i
+              ! use this for delta and delta_vel
+              !
               if (perturb_x) then
                  f = perturb_phi * sin(kx * x(i,j,k) - phi_offset)
                  df1 = perturb_phi * kx * cos(kx * x(i,j,k) - phi_offset)
                  delta_vel(i,j,k,1) = perturb_v0 * df1
               elseif (perturb_y) then
                  f = perturb_phi * sin(ky * y(i,j,k) - phi_offset)
-                 df2 = perturb_phi * ky * cos(ky * y(i,j,k) - phi_offset) ! for all these cases df only has one component
+                 df2 = perturb_phi * ky * cos(ky * y(i,j,k) - phi_offset)
                  delta_vel(i,j,k,2) = perturb_v0 * df2
               elseif (perturb_z) then
                  f = perturb_phi * sin(kz * z(i,j,k) - phi_offset)
@@ -166,7 +178,7 @@ subroutine FLRW_InitialData (CCTK_ARGUMENTS)
               elseif (perturb_all) then
                  f = perturb_phi * (sin(kx * x(i,j,k) - phi_offset) + sin(ky * y(i,j,k) - phi_offset) + sin(kz * z(i,j,k) - phi_offset))
                  df1 = perturb_phi * kx * cos(kx * x(i,j,k) - phi_offset)
-                 df2 = perturb_phi * ky * cos(ky * y(i,j,k) - phi_offset) ! df is now a vector, this is only used in velocity IC
+                 df2 = perturb_phi * ky * cos(ky * y(i,j,k) - phi_offset)
                  df3 = perturb_phi * kz * cos(kz * z(i,j,k) - phi_offset)
                  delta_vel(i,j,k,1) = perturb_v0 * df1
                  delta_vel(i,j,k,2) = perturb_v0 * df2
@@ -176,19 +188,21 @@ subroutine FLRW_InitialData (CCTK_ARGUMENTS)
               phidot = 0._dp     !  d(phi)/dt
               delta(i,j,k) = perturb_rho0 * f              
            endif
-       
-           !! set up metric, extrinsic curvature, lapse and shift
+
+           !
+           ! set up metric, extrinsic curvature, lapse and shift
+           !
            if (data) then
 
               if (lapse) then
-                 if (FLRW_perturb) then
+                 if (perturb) then
                     alp(i,j,k) = sqrt(1._dp + 2._dp * phi(i,j,k))
                  else
                     alp(i,j,k) = FLRW_lapse_value
                  endif
               endif
 	           
-              if (FLRW_perturb) then
+              if (perturb) then
                  gxx(i,j,k) = asq * (1._dp - 2._dp * phi(i,j,k))
                  gxy(i,j,k) = 0._dp
                  gxz(i,j,k) = 0._dp
@@ -217,8 +231,9 @@ subroutine FLRW_InitialData (CCTK_ARGUMENTS)
                  kyz(i,j,k) = 0._dp
                  kzz(i,j,k) = kvalue
               endif
-
-              ! May also need the derivative of the lapse -- evolution of this is specified in ADMBase.
+              !
+              ! time deriv of lapse -- evolution of this is specified in ADMBase.
+              !
               if (dtlapse) then
                  dtalp(i,j,k) = 0._dp
               end if              
@@ -228,23 +243,21 @@ subroutine FLRW_InitialData (CCTK_ARGUMENTS)
                  betay(i,j,k) = 0._dp
                  betaz(i,j,k) = 0._dp
               end if
-         
+              !
               ! set up  matter variables
+              !
               if (hydro) then
+                 !
                  ! initialise matter to homogeneous values
+                 !
                  rho(i,j,k) = rho0
-                 ! press(i,j,k) = FLRW_K * rho0 ** FLRW_gamma   !! previously. removing these FLRW keywords
-                 press(i,j,k) = 0._dp
+                 press(i,j,k) = 0._dp ! pressure will be overwritten to P=poly_k*rho**poly_gamma in EOS_Omni
                  eps(i,j,k) = 0._dp
                  vel(i,j,k,:) = 0._dp
-
-                 if (FLRW_perturb) then
-
+                 if (perturb) then
                     vel(i,j,k,:) = delta_vel(i,j,k,:)
                     rho(i,j,k) = rho(i,j,k) * ( 1._dp + delta(i,j,k) )
-
                  endif
-
               endif
 
            endif
