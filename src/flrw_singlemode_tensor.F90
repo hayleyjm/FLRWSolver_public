6ca4162341cebf21e46c9a916eff439ef039454f
Hayley Macpherson <hayleymac@Hayleys-MBP.broadband>
2021-04-05 14:31:29 +0100

    added hdot soln to tensor perturb ONLY - not tested

--- src/flrw_singlemode_tensor.F90
+++ src/flrw_singlemode_tensor.F90
@@ -21,8 +21,8 @@ subroutine FLRW_SingleMode_Tensor (CCTK_ARGUMENTS)
   logical   :: perturb_x,perturb_y,perturb_z,perturb_all
 
   CCTK_REAL :: a0,rho0,asq,rhostar,hub,adot,hubdot,boxlen(3)
-  CCTK_REAL :: kx,ky,kz
-  CCTK_REAL :: hxx,hxy,hyy,ksq,nfac
+  CCTK_REAL :: kx,ky,kz,tnow,hsval,hsdotval
+  CCTK_REAL :: hxx,hxy,hyy,dthxx,dthxy,dthyy,ksq,nfac
   CCTK_REAL :: ki(3),modk,kvalue,coskx,sinkx,lambda(3)
   CCTK_INT  :: ncells(3)
   character(len=400) :: warn_message
@@ -35,28 +35,6 @@ subroutine FLRW_SingleMode_Tensor (CCTK_ARGUMENTS)
   call set_logicals(lapse,dtlapse,shift,data,hydro)
 
   !
-  ! set logicals that are specific to this single mode case only
-  !perturb_x = .False.; perturb_y = .False.
-  !perturb_z = .False.; perturb_all = .False.
-
-  !perturb_x   = CCTK_EQUALS (FLRW_perturb_direction, "x")
-  !perturb_y   = CCTK_EQUALS (FLRW_perturb_direction, "y")
-  !perturb_z   = CCTK_EQUALS (FLRW_perturb_direction, "z")
-  !perturb_all = CCTK_EQUALS (FLRW_perturb_direction, "all")
-  !
-  !if (perturb_x) then
-    ! call CCTK_INFO("    Perturbing in the x-direction ONLY ... ")
-  !elseif (perturb_y) then
-    ! call CCTK_INFO("    Perturbing in the y-direction ONLY ... ")
-  !elseif (perturb_z) then
-    ! call CCTK_INFO("    Perturbing in the z-direction ONLY ... ")
-  !elseif (perturb_all) then
-    ! call CCTK_INFO("    Perturbing in all directions ... ")
-  !else
-    ! call CCTK_INFO( "    Perturbing in no directions? " )
-  !endif
-
-  !
   ! set some parameters used in setting the background
   call set_parameters(CCTK_ARGUMENTS,a0,rho0,asq,rhostar,hub,adot,hubdot,boxlen,ncells)
 
@@ -65,15 +43,22 @@ subroutine FLRW_SingleMode_Tensor (CCTK_ARGUMENTS)
   !    -- note: this assumes the k(plus) = k(cross) (i.e. wavenumbers of 2 polarisations are equal)
   !    -- but does not assume the amplitudes of these are the same
   !
-  if (FLRW_init_HL <= 1._dp) then
-     call CCTK_WARN(CCTK_WARN_ALERT,"Please set FLRW_init_HL > 1 for tensor perturbations outside horizon. Initial choice of hdot=0 may not be valid.")
-  endif
+  !if (FLRW_init_HL <= 1._dp) then
+    ! call CCTK_WARN(CCTK_WARN_ALERT,"Please set FLRW_init_HL > 1 for tensor perturbations outside horizon. Initial choice of hdot=0 may not be valid.")
+  !endif
   lambda = boxlen              ! lambda^z only for motion in z-direction, others all zero
   kz     = 2._dp * pi / lambda(3) ! k^z for motion in only z-direction, others all zero
   ki     = 0._dp
   ki(3)  = kz
   modk   = sqrt(ki(1)**2 + ki(2)**2 + ki(3)**2)
-  kx = ki(1); ky = ki(2)!; kz = ki(3)
+  kx = ki(1); ky = ki(2)
+
+  !
+  ! Initial time as set by user
+  tnow     = cctk_initial_time
+  ! get functions hs(eta,k) and hsdot(eta,k)
+  hsval    = hs(CCTK_ARGUMENTS,modk,tnow)
+  hsdotval = hsdot(CCTK_ARGUMENTS,modk,tnow)
 
   !
   ! spatial loop over *local* grid size for this processor
@@ -83,24 +68,25 @@ subroutine FLRW_SingleMode_Tensor (CCTK_ARGUMENTS)
         do i = 1, cctk_lsh(1)
 
            !
-           ! Set components of h_ij based on what kind of perturbation we want
+           ! Set components of h_ij and dt_hij
            !
-           !if (perturb_x) then
-            !  coskx = cos(kx * x(i,j,k))
-             ! sinkx = sin(kx * x(i,j,k))
-           !elseif (perturb_y) then
-            !  coskx = cos(ky * y(i,j,k))
-             ! sinkx = sin(ky * y(i,j,k))
-           !elseif (perturb_z) then
-            !  coskx = cos(kz * z(i,j,k))
-            !  sinkx = sin(kz * z(i,j,k))
-           !elseif (perturb_all) then
+
+
            coskx = cos(kx * x(i,j,k) + ky * y(i,j,k) + kz * z(i,j,k))
            sinkx = sin(kx * x(i,j,k) + ky * y(i,j,k) + kz * z(i,j,k))
            !endif
-           hxx = 0.5_dp * hplus_amplitude * coskx + 0.5_dp * hcross_amplitude * coskx
+           hxx = 0.5_dp * hplus_amplitude * hsval * coskx + &
+           & 0.5_dp * hcross_amplitude * hsval * coskx
            hyy = - hxx
-           hxy = 0.5_dp * hplus_amplitude * sinkx + 0.5_dp * hcross_amplitude * sinkx
+           hxy = 0.5_dp * hplus_amplitude * hsval * sinkx + &
+           & 0.5_dp * hcross_amplitude * hsval * sinkx
+
+           dthxx = 0.5_dp * hplus_amplitude * hsdotval * coskx + &
+           & 0.5_dp * hcross_amplitude * hsdotval * coskx
+           dthyy = - dthxx
+           dthxy = 0.5_dp * hplus_amplitude * hsdotval * sinkx + &
+           & 0.5_dp * hcross_amplitude * hsdotval * sinkx
+
 
            !
            ! set up metric, extrinsic curvature, lapse and shift
@@ -132,10 +118,10 @@ subroutine FLRW_SingleMode_Tensor (CCTK_ARGUMENTS)
               gzz(i,j,k) = asq
 
               kvalue     = - adot * a0 / alp(i,j,k)
-              kxx(i,j,k) = kvalue * (1._dp + hxx)  ! dt(h_ij)=0 initially
-              kxy(i,j,k) = kvalue * hxy
+              kxx(i,j,k) = kvalue * (1._dp + hxx) - 0.5_dp * a0 * dthxx
+              kxy(i,j,k) = kvalue * hxy - 0.5_dp * a0 * dthxy
               kxz(i,j,k) = 0._dp
-              kyy(i,j,k) = kvalue * (1._dp + hyy)  ! dt(h_ij)=0 initially
+              kyy(i,j,k) = kvalue * (1._dp + hyy) - 0.5_dp * a0 * dthyy
               kyz(i,j,k) = 0._dp
               kzz(i,j,k) = kvalue
 
